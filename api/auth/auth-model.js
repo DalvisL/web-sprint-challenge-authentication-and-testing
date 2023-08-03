455e1236bc6d3dc1a77b0417947a0f9a06cb224a
Dalvis <Dalvis.liz27@gmail.com>
2023-08-03 14:50:32 -0400

    added helper function

--- api/auth/auth-model.js
+++ api/auth/auth-model.js
@@ -1,15 +1,15 @@
 const db = require('../../data/dbConfig');
 
+const findById = async (id) => {
+    const user = await db('users').where('user_id', id).first();
+    return user;
+};
+
 const register = async (user) => {
     const [id] = await db('users').insert(user);
     return findById(id);
 };
 
-const login = async (creds) => {
-
-};
-
 module.exports = {
     register,
-    login,
 };
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,17 @@
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+describe('[POST] /api/auth/register', () => {
+  it('should return a 201 status', async () => {
+    const res = await request(server)
+      .post('/api/auth/register')
+      .send({ username: 'test', password: 'test' })
+    expect(res.status).toBe(201)
+  })
 })
