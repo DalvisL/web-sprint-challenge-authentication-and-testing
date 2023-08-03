3f9a8802733bc3d6026bf3a9b22e18be1863ab36
Dalvis <Dalvis.liz27@gmail.com>
2023-08-03 19:28:25 -0400

    fixed login bug

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -2,6 +2,9 @@ const db = require('../../data/dbConfig');
 
 const checkUsernameExists = async (req, res, next) => {
     try {
+        if(!req.body.username) {
+            next()
+        }
         const existing = await db('users').where('username', req.body.username).first();
         if (existing) {
             res.status(401).json({ message: 'username taken', existing: existing });
